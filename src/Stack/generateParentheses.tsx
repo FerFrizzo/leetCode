dccf536d47b686267d525c2989d7dd9fc8c51755
Fernando Frizzo <fernandofrizzo@gmail.com>
2024-01-26 14:50:47 +0000

    some Stack and Backtracking solved problems

---
+++ src/Stack/generateParentheses.tsx
@@ -0,0 +1,20 @@
+export function generateParenthesis(n: number): string[] {
+  const result: string[] = []
+
+  if (n === 0) return []
+  if (n === 1) return ['()']
+
+  function backtrack(s: string, left: number, right: number) {
+    if (left === 0 && right === 0) {
+      result.push(s)
+      return
+    }
+
+    if (left < right) backtrack(s + ')', left, right - 1)
+    if (left > 0) backtrack(s + '(', left - 1, right)
+  }
+
+  backtrack('', n, n)
+  console.log(result)
+  return result
+}
---
+++ test/Stack/generateParentheses.test.tsx
@@ -0,0 +1,15 @@
+import { generateParenthesis } from "../../src/Stack/generateParentheses";
+
+
+describe("Generate Parentheses", () => {
+  it("should return all combinations for 3 parentheses", () => {
+    const result = generateParenthesis(3);
+    expect(result).toEqual(expect.arrayContaining(['()()()', '()(())', '(())()', '(()())', '((()))']));
+  })
+
+  it("should return a single parenthesis for 1 combination", () => {
+    const result = generateParenthesis(1);
+    expect(result).toBe(['()']);
+  })
+
+});
